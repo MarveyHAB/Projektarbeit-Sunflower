0fb693e91ada85a35f3bd2fe00168e5928e335e9
Marvin <k.reichenbach@students.rak.de>
2023-10-09 18:03:22 +0200

    anpassungen VSS

--- main.py
+++ main.py
@@ -3,24 +3,33 @@ import ds1307
 from hmc5883l import HMC5883L
 from sonne_jan import getAZ, getSEA
 from drehen import step #sunPos
-from machine import Pin, RTC, SoftI2C
+from machine import Pin, RTC, SoftI2C, Timer
 from time import sleep, sleep_ms
 
-btn_start         = Pin(22 , Pin.IN,PULL_DOWN)
-btn_stopp         = Pin(26 , Pin.IN,PULL_DOWN)
+#Timer
+ausrichtung_check = Timer()
+
+#BTN
+btn_startstopp    = Pin(22 , Pin.IN,PULL_DOWN)
+btn_NOTStopp      = Pin(26 , Pin.IN,PULL_DOWN)
 btn_einfahren     = Pin(27 , Pin.IN,PULL_DOWN)
 ini_ausgefaechert = Pin(21 , Pin.IN,PULL_DOWN)
-anlage_ein = False
+anlage_ein        = False
 
-def ablauf(JackeWieHose):
+def ISR_ausrichtung(abc):
+    #Sonnenstand aktualisieren
+    #Positionen checken ggf. neu verfahren
+    
+
+def ablauf(pin22):
     global anlage_ein
-    anlage_ein = True
-    while anlage_ein is True:
-        AZ = getAZ(51,7,2)
-        SE = getSEA(51,7,2)
-        print("Sonnenrichtung:", AZ)
-        print("Sonnenhöhe:" ,SE)
+    anlage_ein = not anlage_ein
+    if(anlage_ein == True):
+        ausrichtung_check.init(period=120000 , mode=Timer.PERIODIC, callback=ISR_ausrichtung)
+    else
+        ausrichtung_check.deinit()
         
+    while anlage_ein is True:
         if ini_ausgefaechert != 1:
             {
                 #neigen 90grad
@@ -30,14 +39,17 @@ def ablauf(JackeWieHose):
             #neigen auf Sonnenhöhe
             }
         else:
-            step(int(AZ))
+            step(int(AZ))#
+            
             #neigen auf Sonnenhöhe
         sleep(120)
-        
-def stopp(JackeWieHose):
+    ausrichtung_check.deinit()
+    
+def NOTStopp(pin26):
     global anlage_ein
     anlage_ein = False
+    ausrichtung_check.deinit()
 
-btn_start.irq(trigger=Pin.IRQ_FALLING, handler=ablauf)
-btn_stopp.irq(trigger=Pin.IRQ_FALLING, handler=stopp)
+btn_startstopp.irq(trigger=Pin.IRQ_FALLING, handler=ablauf)
+btn_NOTStopp.irq(trigger=Pin.IRQ_FALLING, handler=NOTStopp)
 
