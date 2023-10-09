2106cda3f68a424540c15718e8be3675de8dd27a
JanDerCoder <145771197+JanDerCoder@users.noreply.github.com>
2023-10-09 10:12:54 +0200

    Update main.py
    einmal alles geändert

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
-'''from time import sleep_ms
-from machine import Pin, SoftI2C
-
-i2c  =  SoftI2C( scl=Pin(1) , sda=Pin(0))
-devices = i2c.scan()
-
-if len(devices) == 0:
-  print("No i2c device !")
-else:
-  print('i2c devices found:',len(devices))
- 
-  for device in devices:  
-    print("Decimal address: ",device," | Hexa address: ",hex(device))
-'''
 import veml7700
 from hmc5883l import HMC5883L
-from Sonne import getAZ, getSEA
-
+from sonne_jan import getAZ, getSEA
+from drehen import step #sunPos
+from machine import Pin
 from time import sleep, sleep_ms
-from machine import Pin, SoftI2C
 
-#i2c  =  SoftI2C( scl=Pin(1) , sda=Pin(0))
-#veml = veml7700.VEML7700(address=0x10, i2c=i2c, it=100, gain=1/8)
+btn_start         = Pin(22 , Pin.IN,PULL_DOWN)
+btn_stopp         = Pin(26 , Pin.IN,PULL_DOWN)
+btn_einfahren     = Pin(27 , Pin.IN,PULL_DOWN)
+ini_ausgefaechert = Pin(21 , Pin.IN,PULL_DOWN)
+anlage_ein = False
 
-while True:
-    AZ = getAZ(51,7,2)
-    SE = getSEA(51,7,2)
-    print("Sonnenrichtung:", AZ)
-    print("Sonnenhöhe:" ,SE)
-    sleep(10)
-    '''
-    veml = veml7700.VEML7700(address=0x10, i2c=i2c, it=100, gain=1/8)
-    lux_val = veml.read_lux()
-    print("Helligkeit in lux: ",lux_val)
-    sleep_ms(500)
-    '''
+def ablauf(JackeWieHose):
+    global anlage_ein
+    anlage_ein = True
+    while anlage_ein is True:
+        AZ = getAZ(51,7,2)
+        SE = getSEA(51,7,2)
+        print("Sonnenrichtung:", AZ)
+        print("Sonnenhöhe:" ,SE)
+        
+        if ini_ausgefaechert != 1:
+            {
+                #neigen 90grad
+            #auffächern
+            #drehen:
+            step(int(AZ))
+            #neigen auf Sonnenhöhe
+            }
+        else:
+            step(int(AZ))
+            #neigen auf Sonnenhöhe
+        sleep(120)
+        
+def stopp(JackeWieHose):
+    global anlage_ein
+    anlage_ein = False
+
+btn_start.irq(trigger=Pin.IRQ_FALLING, handler=ablauf)
+btn_stopp.irq(trigger=Pin.IRQ_FALLING, handler=stopp)
+
