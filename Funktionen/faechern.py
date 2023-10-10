f6b5f430967fbe0ea0e5a16cf04b36af5d1ad663
JanDerCoder <145771197+JanDerCoder@users.noreply.github.com>
2023-10-10 08:47:56 +0200

    erste Version faechern.py
    könnte vielleicht funktionieren

--- Funktionen/faechern.py
+++ Funktionen/faechern.py
@@ -1,3 +1,8 @@
+from machine import Pin
+from time import sleep
+
+ini_eingefaechert = Pin(20 , Pin.IN,PULL_DOWN)
+ini_ausgefaechert = Pin(21 , Pin.IN,PULL_DOWN)
 brake     = Pin(13 , Pin.OUT,value =0)
 # Pins für den DRV8825 Schrittmotor-Treiber
 DIR_PIN   = Pin(8 , Pin.OUT,value =0)  # Richtungs-Pin 1 Uhrzeigersinn
@@ -7,3 +12,51 @@ SLEEP_PIN = Pin(10, Pin.OUT,value =1)  # Aktivierung des Treibers
 # Mikroschritt-Modus 0 = 0
 # Mikroschritt-Modus 1 = 1
 # Mikroschritt-Modus 2 = 0
+
+def auffaechern:
+    if ini_eingefaechert==0:
+        print ("Unbestimmte Pos")
+        return(False)
+    DIR_PIN.value(1) #überprüfen
+    steps = int(360/(1.8/(4*30)))
+    # Schalte den Treiber ein
+    SLEEP_PIN.value(1)
+    sleep(.5)
+    brake.value(1)
+    sleep(.5)
+    for _ in range(steps):
+        # Mache einen Schritt
+        STEP_PIN.value(1)
+        sleep_us(500)  # Wartezeit vor dem nächsten Schritt
+        STEP_PIN.value(0)
+        sleep_us(500)  # Wartezeit nach dem Schritt
+        if ini_ausgefaechert == 1:
+            break 
+    brake.value(0)
+    sleep(0.5)
+    SLEEP_PIN.value(0)
+    
+def einfaechern:
+    if ini_ausgefaechert==0:
+        print ("Unbestimmte Pos")
+        return(False)
+    DIR_PIN.value(0) #überprüfen
+    steps = int(360/(1.8/(4*30)))
+    # Schalte den Treiber ein
+    SLEEP_PIN.value(1)
+    sleep(.5)
+    brake.value(1)
+    sleep(.5)
+    for _ in range(steps):
+        # Mache einen Schritt
+        STEP_PIN.value(1)
+        sleep_us(500)  # Wartezeit vor dem nächsten Schritt
+        STEP_PIN.value(0)
+        sleep_us(500)  # Wartezeit nach dem Schritt
+        if ini_eingefaechert == 1:
+            break 
+    brake.value(0)
+    sleep(.5)
+    SLEEP_PIN.value(0)
+        
+
