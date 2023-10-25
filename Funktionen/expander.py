1d4031d0005a9419ffb62245b43b346a331d4e5d
MarvinDerCoder <43411557+MarveyHAB@users.noreply.github.com>
2023-10-25 16:32:18 +0200

    Update expander.py

--- Funktionen/expander.py
+++ Funktionen/expander.py
@@ -6,6 +6,25 @@ i2c.writeto(0x70, b'\xF')
 i2c.scan()
 # [60, 112] - Display 1 (SSD1306) & TCA9548A
 
+# enable channel 0 (SD0,SC0)
+i2c.writeto(0x70, b'\x01')
+i2c.scan()
+
+
+# enable channel 1 (SD1,SC1)
+i2c.writeto(0x70, b'\x01')
+i2c.scan()
+
+# enable channel 2 (SD2,SC2)
+i2c.writeto(0x70, b'\x01')
+i2c.scan()
+
+# enable channel 3 (SD3,SC3)
+i2c.writeto(0x70, b'\x01')
+i2c.scan()
+
+
+
 # read which channels are enabled?
 i2c.readfrom(0x70, 1)
 # b'\x05' - channels 0 and 2 (0x05 == 0b_0000_0101)
