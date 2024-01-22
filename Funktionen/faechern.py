cfe89dd840b752b529daf3c77b6a73d2f4194299
JanDerCoder <145771197+JanDerCoder@users.noreply.github.com>
2024-01-22 10:26:19 +0100

    Update faechern.py

--- Funktionen/faechern.py
+++ Funktionen/faechern.py
@@ -8,7 +8,7 @@ ini_ausgefaechert = Pin(19 , Pin.IN,Pin.PULL_UP)
 auf               = Pin(26 , Pin.IN,Pin.PULL_UP)
 zu                = Pin(27 , Pin.IN,Pin.PULL_UP)
 # Pins für den DRV8825 Schrittmotor-Treiber
-DIR_PIN   = Pin(8 , Pin.OUT,value =0)  # Richtungs-Pin 1 Uhrzeigersinn
+DIR_PIN   = Pin(8 , Pin.OUT,value =0)  # 1 = Zufächern 0= Auffächern
 STEP_PIN  = Pin(9 , Pin.OUT,value =0)  # Schritt-Pin
 SLEEP_PIN = Pin(10, Pin.OUT,value =0)  # Aktivierung des Treibers
 PSU24V = Pin(12 , Pin.OUT, value=0)
@@ -19,16 +19,127 @@ micro_step = 4 #1/4
 ratio      = 30
 time_step  = 1500
 
-#wieder löschen
-from _thread import allocate_lock
-NOTHALT = allocate_lock()
 
-#fehler 20 fehler kalibrierung
 def faechern_kali(NOTHALT):
+    return 0 #weil fächern nicht getestet werden kann
+    print("wird aufgefaechert")
+    
+    if ini_ausgefaechert.value() == 0:
+        print("Ist aufgefächert!")
+        return 0
+    
+    steps = round((318/1.8)*micro_step*ratio)# Eigentlich 315° Fächern +3 für Toleranz
+    
+    DIR_PIN.value(0)
+    SLEEP_PIN.value(1)
+    PSU24V.value(1)
+    sleep(.5)
+    steps=500 #wieder rausnehmen mit inis
+    
+    for _ in range(steps):
+        if NOTHALT.locked()== True:
+            break
+        STEP_PIN.value(1)
+        sleep_us(int(time_step/2))
+        STEP_PIN.value(0)
+        sleep_us(int(time_step/2))
+        if ini_ausgefaechert.value() == 0:
+            print ("Ini ausgefächert angesprochen")
+            break
+            #bzw nur noch ein paar schritte
+    sleep(.5)
+    SLEEP_PIN.value(0)
+    PSU24V.value(0)
+    
+    if NOTHALT.locked()== True:
+        print("Not-Halt hat ausgelöst")
+        return 41
+    
+    if ini_ausgefaechert.value() == 1:
+        print("Ini ist nicht angesprochen, Kalibirierung fehlgeschlagen!")
+        return 20
+    
+    print("Kalibrierung fächern erfolgreich")
+    return 0  
+
+
+def auffaechern(NOTHALT):
+    return 0 #weil fächern nicht getestet werden kann
+    print("wird aufgefaechert")
+    
+    steps = round((318/1.8)*micro_step*ratio)# Eigentlich 315° Fächern +3 für Toleranz
+    
+    DIR_PIN.value(0)
+    SLEEP_PIN.value(1)
+    PSU24V.value(1)
+    sleep(.5)
+    steps=500 #wieder rausnehmen mit inis
+    
+    for _ in range(steps):
+        if NOTHALT.locked()== True:
+            break
+        STEP_PIN.value(1)
+        sleep_us(int(time_step/2))
+        STEP_PIN.value(0)
+        sleep_us(int(time_step/2))
+        if ini_ausgefaechert.value() == 0:
+            print ("Ini ausgefächert angesprochen")
+            break
+            #bzw nur noch ein paar schritte
+    sleep(.5)
+    SLEEP_PIN.value(0)
+    PSU24V.value(0)
+    
+    if NOTHALT.locked()== True:
+        print("Not-Halt hat ausgelöst")
+        return 41
+    
+    if ini_ausgefaechert.value() == 1:
+        print("Ini ist nicht angesprochen, muss von Hand angefahren werden!")
+        return 21
+    
+    return 0
+    
+
+def einfaechern(NOTHALT):
+    return 0 #weil fächern nicht getestet werden kann
+    print("wird eingefaechert")
+    
+    DIR_PIN.value(1)
+    steps = round((318/1.8)*micro_step*ratio) # Eigentlich 315° Fächern +3 für Toleranz
+    
+    SLEEP_PIN.value(1)
+    PSU24V.value(1)
+    sleep(.5)
+    steps=150 #noch loeschen
+    for _ in range(steps):
+        if NOTHALT.locked()== True:
+            break
+        STEP_PIN.value(1)
+        sleep_us(int(time_step/2))
+        STEP_PIN.value(0)
+        sleep_us(int(time_step/2))
+        if ini_eingefaechert.value() == 0:
+            break
+            #bzw nur noch ein paar schritte
+    sleep(.5)
+    SLEEP_PIN.value(0)
+    PSU24V.value(0)
+    
+    if NOTHALT.locked()== True:
+        print("Not-Halt hat ausgelöst")
+        return 41
+    
+    if ini_eingefaechert.value() == 1:
+        print("Ini ist nicht angesprochen, muss von Hand angefahren werden!")
+        return 22
+    
+    print("eingefaechert")
+    
     return 0
 
 
-def faechern_hand(NOTHALT):
+def faechern_hand():
     
     while auf.value()==1 or zu.value()==1:
         
@@ -96,74 +207,10 @@ def faechern_hand(NOTHALT):
             PSU24V.value(0)
             SLEEP_PIN.value(0)
             sleep(.2)
-        
-
-#Fehler 21 fehler auffächern
-def auffaechern(NOTHALT):
-    return 0 #weil fächern nicht getestet werden kann
-    print("wird aufgefaechert")
-    #if ini_eingefaechert.value()==1:
-    #    print ("Unbestimmte Pos und wird deswegen doch nicht aufgefächert")
-    #    return(False)
-    
-    DIR_PIN.value(0) #überprüfen
-    steps = round((360/1.8)*micro_step*ratio)
-    print("360grad sind %i steps"%steps)
-    print("einfahren dauert %i s"%(int(steps* time_step*0.000001)))
-    # Schalte den Treiber ein
-    SLEEP_PIN.value(1)
-    PSU24V.value(1)
-    sleep(.5)
-    steps=500
-    for _ in range(steps):
-        # Mache einen Schritt
-        STEP_PIN.value(1)
-        sleep_us(int(time_step/2))  # Wartezeit vor dem nächsten Schritt
-        STEP_PIN.value(0)
-        sleep_us(int(time_step/2))  # Wartezeit nach dem Schritt
-        if ini_ausgefaechert.value() == 0:
-            print ("Ini ausgefächert angesprochen")
-            break
-            #bzw nur noch ein paar schritte
-    sleep(0.5)
-    SLEEP_PIN.value(0)
-    PSU24V.value(0)
-    print("aufgefaechert")
-
-#fehler 22 einfächern
-def einfaechern(NOTHALT):
-    return 0 #weil fächern nicht getestet werden kann
-    print("wird eingefaechert")
-    #if ini_ausgefaechert.value()==1:
-    #    print ("Unbestimmte Pos und wird deswegen doch nicht aufgefächert")
-    #    return(False)
-    
-    DIR_PIN.value(1) #überprüfen
-    steps = round((360/1.8)*micro_step*ratio)
-    print("360grad sind %i steps"%steps)
-    print("einfahren dauert %i s"%(int(steps* time_step*0.000001)))
-    # Schalte den Treiber ein
-    SLEEP_PIN.value(1)
-    PSU24V.value(1)
-    sleep(.5)
-    steps=150
-    for _ in range(steps):
-        # Mache einen Schritt
-        STEP_PIN.value(1)
-        sleep_us(int(time_step/2))  # Wartezeit vor dem nächsten Schritt
-        STEP_PIN.value(0)
-        sleep_us(int(time_step/2))  # Wartezeit nach dem Schritt
-        if ini_eingefaechert.value() == 0:
-            break
-            #bzw nur noch ein paar schritte
-    sleep(0.5)
-    SLEEP_PIN.value(0)
-    PSU24V.value(0)
-    print("eingefaechert")
-    
-    
+    return 0    
     
 #einfaechern(NOTHALT)
 #auffaechern(NOTHALT)
-faechern_hand(NOTHALT)
+#faechern_hand()
+
 
