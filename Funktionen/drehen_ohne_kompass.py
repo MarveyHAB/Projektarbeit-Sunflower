5e041a1e82a9b6e942a969737127bd173e11804d
JanDerCoder <145771197+JanDerCoder@users.noreply.github.com>
2024-01-22 10:25:23 +0100

    Update drehen_ohne_kompass.py

--- Funktionen/drehen_ohne_kompass.py
+++ Funktionen/drehen_ohne_kompass.py
@@ -1,6 +1,5 @@
 from machine    import Pin
 from time       import sleep_us,sleep,sleep_ms
-#from hmc5883l import HMC5883L
 from Sonne      import getAZ
 
 ini_grund_pos	= Pin(16 , Pin.IN,Pin.PULL_UP)
@@ -10,17 +9,14 @@ links			= Pin(27 , Pin.IN,Pin.PULL_UP)
 
 # Pins für den DRV8825 Schrittmotor-Treiber
 DIR_PIN   = Pin(2, Pin.OUT,value =0)  # Richtungs-Pin 1 Uhrzeigersinn
-STEP_PIN  = Pin(3, Pin.OUT,value =0)  # Schritt-Pin
-SLEEP_PIN = Pin(4, Pin.OUT,value =0)  # Aktivierung des Treibers
-PSU_24V   = Pin (12, Pin.OUT,value =0)#24 aktivieren
+STEP_PIN  = Pin(3, Pin.OUT,value =0)  
+SLEEP_PIN = Pin(4, Pin.OUT,value =0)  
+PSU_24V   = Pin (12, Pin.OUT,value =0)
 
 micro_step = 4 		#1/4
-ratio      = 50		#gerade kein Getriebe am start
+ratio      = 50		
 time_step  =2500 	#in us
 grund_pos = 180
-
-#from _thread import allocate_lock
-#NOTHALT = allocate_lock()
 
 
 def drehen_kali(NOTHALT):
@@ -60,8 +56,6 @@ def drehen_kali(NOTHALT):
     sleep(.2)
     ini_angesprochen = False
     
-    #Nothalt einfügen
-    #und falls zu viele schritte gemacht werden
     for _ in range(steps):
         if NOTHALT.locked()== True:
             break
@@ -73,6 +67,7 @@ def drehen_kali(NOTHALT):
             print ("Ini Drehen Grundpos. angesprochen")
             break
         
+    sleep(.2)
     PSU_24V.value(0)
     SLEEP_PIN.value(0)
     brake.value(0)
@@ -90,8 +85,131 @@ def drehen_kali(NOTHALT):
         print("Muss im Handbetrieb angefahren werden, da er die pos. nicht findet.")
         return 30,180
         
-        
-def drehen_hand(NOTHALT):
+
+def drehen_grundpos(NOTHALT,pos):
+    PSU_24V.value(1)
+    #aktuelle Position herausfinden
+    with open("totale_pos.txt","r") as datei:
+        inhalt  = datei.read()
+        abs_pos = int(inhalt)
+    akt_pos = abs_pos
+    print("aktuelle Pos.:%i"%akt_pos)
+    if akt_pos == grund_pos:
+        print("aktuelle Pos. entspricht noch der Grundpos.")
+        return 0,pos
+    # Berechnung des kürzesten Weges im Uhrzeigersinn
+    im_uhrzeigersinn = (grund_pos - akt_pos) % 360
+    # Berechnung des kürzesten Weges gegen den Uhrzeigersinn
+    gegen_uhrzeigersinn = (akt_pos - grund_pos) % 360
+    #was ist kürzer
+    if im_uhrzeigersinn<gegen_uhrzeigersinn:
+        DIR_PIN.value(0) #im Uhrzeigersinn
+        print("im Uhrzeigersinn sind es %i grad"%im_uhrzeigersinn)
+        dif=im_uhrzeigersinn
+        if 360<akt_pos+dif:
+            akt_pos=akt_pos-360+dif
+        else:
+            akt_pos=akt_pos+dif
+    else:
+        DIR_PIN.value(1) #gegen den Uhrzeigersinn
+        print("gegen Uhrzeigersinn sind es %i grad"%gegen_uhrzeigersinn)
+        dif=gegen_uhrzeigersinn
+        if 0>akt_pos-dif:
+            akt_pos=akt_pos+360-dif
+        else:
+            akt_pos=akt_pos-dif
+    #+5° damit er den Ini auf jeden Fall erreicht, wenn nicht liegt vermutlich ein Fehler vor
+    steps = round(((dif+5)/1.8)*micro_step*ratio)
+
+    SLEEP_PIN.value(1)
+    sleep(.2)
+    brake.value(1)
+    sleep(.2)
+    ini_angesprochen = False
+    for _ in range(steps):
+        STEP_PIN.value(1)
+        sleep_us(round(time_step/2))
+        STEP_PIN.value(0)
+        sleep_us(round(time_step/2))
+        if ini_grund_pos.value() == 0:
+            print ("Ini Drehen Grundpos. angesprochen")
+            ini_angesprochen = True
+            break
+    brake.value(0)
+    sleep(.2)
+    SLEEP_PIN.value(0)
+    PSU_24V.value(0)
+    
+    print("angefahrene Pos. %i"%akt_pos)
+    with open("totale_pos.txt","w") as datei:
+            datei.write(str(akt_pos))
+    
+    if ini_angesprochen == False:
+        print("Ini Grundpos. wurde nicht erreicht")
+        return 31,180 #anpassen
+    
+    return 0,180
+
+
+def drehen_sonne(NOTHALT,pos):
+    PSU_24V.value(1)
+    #aktuelle Sonnenposition abfragen
+    sunPos = int(getAZ(51,7,2))
+    print("Sonnenposition: %i"%sunPos)
+    #aktuelle Position herausfinden
+    with open("totale_pos.txt","r") as datei:
+        inhalt  = datei.read()
+        abs_pos = int(inhalt)
+    akt_pos = abs_pos
+    print("aktuelle Pos.:%i"%akt_pos)
+    if akt_pos == sunPos:
+        print("aktuelle Pos. entspricht noh der Sonnenpos.")
+        return(0,pos)
+    # Berechnung des kürzesten Weges im Uhrzeigersinn
+    im_uhrzeigersinn = (sunPos - akt_pos) % 360
+    # Berechnung des kürzesten Weges gegen den Uhrzeigersinn
+    gegen_uhrzeigersinn = (akt_pos - sunPos) % 360
+    #was ist kürzer
+    if im_uhrzeigersinn<gegen_uhrzeigersinn:
+        DIR_PIN.value(0) #im Uhrzeigersinn
+        print("im Uhrzeigersinn sind es %i grad"%im_uhrzeigersinn)
+        dif=im_uhrzeigersinn
+        if 360<akt_pos+dif:
+            akt_pos=akt_pos-360+dif
+        else:
+            akt_pos=akt_pos+dif
+    else:
+        DIR_PIN.value(1) #gegen den Uhrzeigersinn
+        print("gegen Uhrzeigersinn sind es %i grad"%gegen_uhrzeigersinn)
+        dif=gegen_uhrzeigersinn
+        if 0>akt_pos-dif:
+            akt_pos=akt_pos+360-dif
+        else:
+            akt_pos=akt_pos-dif
+    #getriebeübersetzug 1/30 und 1/4 multistepping
+    steps = round((dif/1.8)*micro_step*ratio)
+    # Schalte den Treiber ein
+    SLEEP_PIN.value(1)
+    sleep(.2)
+    brake.value(1)
+    sleep(.5)
+    for _ in range(steps):
+        # Mache einen Schritt
+        STEP_PIN.value(1)
+        sleep_us(round(time_step/2))  # Wartezeit vor dem nächsten Schritt
+        STEP_PIN.value(0)
+        sleep_us(round(time_step/2))  # Wartezeit nach dem Schritt
+    brake.value(0)
+    sleep(.2)
+    SLEEP_PIN.value(0)
+    print("angefahrene Pos. %i"%akt_pos)
+    with open("totale_pos.txt","w") as datei:
+            datei.write(str(akt_pos))
+    PSU_24V.value(0)
+    return 0,akt_pos
+
+
+def drehen_hand():
     while rechts.value()==1 or links.value()==1:
         
         first_run_rechts   = True
@@ -150,132 +268,10 @@ def drehen_hand(NOTHALT):
             PSU24V.value(0)
             SLEEP_PIN.value(0)
             sleep(.2)
-
-
-def drehen_grundpos(NOTHALT,pos):
-    PSU_24V.value(1)
-#aktuelle Position herausfinden
-    with open("totale_pos.txt","r") as datei:
-        inhalt  = datei.read()
-        abs_pos = int(inhalt)
-    akt_pos = abs_pos
-    print("aktuelle Pos.:%i"%akt_pos)
-    if akt_pos == grund_pos:
-        print("aktuelle Pos. entspricht noch der Grundpos.")
-        return 0,pos
-# Berechnung des kürzesten Weges im Uhrzeigersinn
-    im_uhrzeigersinn = (grund_pos - akt_pos) % 360
-# Berechnung des kürzesten Weges gegen den Uhrzeigersinn
-    gegen_uhrzeigersinn = (akt_pos - grund_pos) % 360
-#was ist kürzer
-    if im_uhrzeigersinn<gegen_uhrzeigersinn:
-        DIR_PIN.value(0) #im Uhrzeigersinn
-        print("im Uhrzeigersinn sind es %i grad"%im_uhrzeigersinn)
-        dif=im_uhrzeigersinn
-        if 360<akt_pos+dif:
-            akt_pos=akt_pos-360+dif
-        else:
-            akt_pos=akt_pos+dif
-    else:
-        DIR_PIN.value(1) #gegen den Uhrzeigersinn
-        print("gegen Uhrzeigersinn sind es %i grad"%gegen_uhrzeigersinn)
-        dif=gegen_uhrzeigersinn
-        if 0>akt_pos-dif:
-            akt_pos=akt_pos+360-dif
-        else:
-            akt_pos=akt_pos-dif
-    #+5° damit er den Ini auf jeden Fall erreicht, wenn nicht liegt vermutlich ein Fehler vor
-    steps = round(((dif+5)/1.8)*micro_step*ratio)
-    # Schalte den Treiber ein
-    SLEEP_PIN.value(1)
-    sleep(.2)
-    brake.value(1)
-    sleep(.2)
-    ini_angesprochen = False
-    for _ in range(steps):
-        # Mache einen Schritt
-        STEP_PIN.value(1)
-        sleep_us(round(time_step/2))  # Wartezeit vor dem nächsten Schritt
-        STEP_PIN.value(0)
-        sleep_us(round(time_step/2))  # Wartezeit nach dem Schritt
-        if ini_grund_pos.value() == 0:
-            print ("Ini Drehen Grundpos. angesprochen")
-            ini_angesprochen = True
-            break
-    brake.value(0)
-    sleep(.2)
-    SLEEP_PIN.value(0)
-    PSU_24V.value(0)
-    
-    print("angefahrene Pos. %i"%akt_pos)
-    with open("totale_pos.txt","w") as datei:
-            datei.write(str(akt_pos))
-    
-    if ini_angesprochen == False:
-        print("Ini Grundpos. wurde nicht erreicht")
-        return 31,180 #anpassen
-    
-    return 0,180
-
-
-def drehen_sonne(NOTHALT,pos):
-    PSU_24V.value(1)
-#aktuelle Sonnenposition abfragen
-    sunPos = int(getAZ(51,7,2))
-    print("Sonnenposition: %i"%sunPos)
-#aktuelle Position herausfinden
-    with open("totale_pos.txt","r") as datei:
-        inhalt  = datei.read()
-        abs_pos = int(inhalt)
-    akt_pos = abs_pos
-    print("aktuelle Pos.:%i"%akt_pos)
-    if akt_pos == sunPos:
-        print("aktuelle Pos. entspricht noh der Sonnenpos.")
-        return(0,pos)
-# Berechnung des kürzesten Weges im Uhrzeigersinn
-    im_uhrzeigersinn = (sunPos - akt_pos) % 360
-# Berechnung des kürzesten Weges gegen den Uhrzeigersinn
-    gegen_uhrzeigersinn = (akt_pos - sunPos) % 360
-#was ist kürzer
-    if im_uhrzeigersinn<gegen_uhrzeigersinn:
-        DIR_PIN.value(0) #im Uhrzeigersinn
-        print("im Uhrzeigersinn sind es %i grad"%im_uhrzeigersinn)
-        dif=im_uhrzeigersinn
-        if 360<akt_pos+dif:
-            akt_pos=akt_pos-360+dif
-        else:
-            akt_pos=akt_pos+dif
-    else:
-        DIR_PIN.value(1) #gegen den Uhrzeigersinn
-        print("gegen Uhrzeigersinn sind es %i grad"%gegen_uhrzeigersinn)
-        dif=gegen_uhrzeigersinn
-        if 0>akt_pos-dif:
-            akt_pos=akt_pos+360-dif
-        else:
-            akt_pos=akt_pos-dif
-    #getriebeübersetzug 1/30 und 1/4 multistepping
-    steps = round((dif/1.8)*micro_step*ratio)
-    # Schalte den Treiber ein
-    SLEEP_PIN.value(1)
-    sleep(.2)
-    brake.value(1)
-    sleep(.5)
-    for _ in range(steps):
-        # Mache einen Schritt
-        STEP_PIN.value(1)
-        sleep_us(round(time_step/2))  # Wartezeit vor dem nächsten Schritt
-        STEP_PIN.value(0)
-        sleep_us(round(time_step/2))  # Wartezeit nach dem Schritt
-    brake.value(0)
-    sleep(.2)
-    SLEEP_PIN.value(0)
-    print("angefahrene Pos. %i"%akt_pos)
-    with open("totale_pos.txt","w") as datei:
-            datei.write(str(akt_pos))
-    PSU_24V.value(0)
-    return 0,akt_pos
+    return 0
 
 #drehen_sonne(NOTHALT,pos)
 #drehen_grundpos(NOTHALT,pos)
 #drehen_kali(NOTHALT)
 #drehen_hand(NOTHALT)
+
